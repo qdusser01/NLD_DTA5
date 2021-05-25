3db4e63e7d0d19fdb7aa64dd657672782d1f3e59
unknown <qdusser@qualixo.com>
2021-05-25 15:51:46 +0200

    first commit

---
+++ scripts/jsAction_3f439923-6f18-4d64-8e4b-8a7a70c25ca8.js
@@ -0,0 +1,17 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var Item = context.variableManager.getValue("item");
+if (Item==null) {
+        context.fail("Variable 'item' not found");
+}
+
+var time = context.variableManager.getValue("time");
+if (time==null) {
+        context.fail("Variable 'time' not found");
+}
+
+logger.debug("The item is: "+Item+" The time is: "+time+" The LG is: "+context.currentLG.name);
